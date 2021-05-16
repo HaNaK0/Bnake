bad6416c59ea9c70639ddb3cb234cb856d01a48d
HaNaK0 <saturn-v@hotmail.com>
2021-05-16 11:33:04 +0200

    Add empty bevy app

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use bevy::prelude::*;
 
 fn main() {
+    App::build()
+        .add_startup_system(setup.system())
+        .add_plugins(DefaultPlugins)
+        .run();   
+}
 
-        
+fn setup(
+    mut commands: Commands
+) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
