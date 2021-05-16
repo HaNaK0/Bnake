1c4af1ea566272fb8a5bda2bd4768c7672f4512f
HaNaK0 <saturn-v@hotmail.com>
2021-05-16 14:50:31 +0200

    Add input

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     App::build()
         .add_startup_system(setup.system())
         .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
+        .add_system(snake_movement.system().label("input"))
         .add_plugins(DefaultPlugins)
         .run();   
 }
@@ -17,6 +18,7 @@ fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>
 ) {
+    info!("Hello World!");
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.insert_resource(Materials {
         head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
@@ -35,3 +37,24 @@ fn spawn_snake(
         })
         .insert(SnakeHead);
 }
+
+fn snake_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+) {
+    for mut transform in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Left) {
+            transform.translation.x -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Right) {
+            transform.translation.x += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Up){
+            transform.translation.y += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Down) {
+            transform.translation.y -= 2.;
+        }
+    }
+}
+
