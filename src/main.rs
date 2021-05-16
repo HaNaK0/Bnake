79676a95cb2e11c893ad973763192d39e6bd2652
HaNaK0 <saturn-v@hotmail.com>
2021-05-16 16:04:29 +0200

    Slapping a grid on it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
-use bevy::prelude::*;
+use core::f32;
+
+use bevy::{prelude::*};
+
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
+
+#[derive(Default, Copy, Clone, Eq, PartialEq, Hash)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
 
 struct SnakeHead;
 struct Materials{
@@ -10,6 +35,12 @@ fn main() {
         .add_startup_system(setup.system())
         .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
         .add_system(snake_movement.system().label("input"))
+        .add_system_set_to_stage(
+            CoreStage::PostUpdate, 
+            SystemSet::new()
+                .with_system(size_scaling.system())
+                .with_system(position_translation.system()),
+        )
         .add_plugins(DefaultPlugins)
         .run();   
 }
@@ -35,7 +66,9 @@ fn spawn_snake(
             sprite: Sprite::new(Vec2::new(10.0, 10.0)),
             ..Default::default()
         })
-        .insert(SnakeHead);
+        .insert(SnakeHead)
+        .insert(Position{x: 0, y: 0})
+        .insert(Size::square(0.8));
 }
 
 fn snake_movement(
@@ -58,3 +91,33 @@ fn snake_movement(
     }
 }
 
+fn size_scaling(
+    windows: Res<Windows>, 
+    mut q: Query<(&Size, &mut Sprite)>
+) {
+    let window = windows.get_primary().unwrap();
+    for (sprite_size, mut sprite) in q.iter_mut() {
+        sprite.size = Vec2::new(
+            sprite_size.width / ARENA_WIDTH as f32 * window.width(), 
+            sprite_size.height / ARENA_HEIGHT as f32 * window.height(),
+        )
+    }
+}
+
+fn position_translation(
+    windows: Res<Windows>,
+    mut q: Query<(&Position, &mut Transform)>,
+) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game - (bound_window / 2.) + (tile_size / 2.)
+    }
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32), 
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32), 
+            0.0
+        )
+    }
+}
